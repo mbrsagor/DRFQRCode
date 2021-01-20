781d333a0ef0aebaa676d6b02a508924ac9b61f6
Sagor <mbrsagor@gmail.com>
2021-01-20 12:09:46 +0600

    User registration console open for development purpose

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -8,10 +8,10 @@ export const register = (user, history) => (dispatch) => {
   Axios.post("http://127.0.0.1:8000/api/register", user)
     .then((res) => {
       history.push("/login");
-    //   console.log(res);
+      console.log(res.data);
     })
     .catch((error) => {
-      //   console.log(error)
+        console.log(error)
       dispatch({
         type: Types.USERS_ERROR,
         payload: {
