f9c70696d3f4188a72f742a4c7ade223a9b55d98
Sagor <mbrsagor@gmail.com>
2021-01-21 02:11:16 +0600

    Basic permission

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {listTypes } from '../store/actions/typeAction';
 
@@ -16,17 +16,18 @@ class Dashboard extends Component {
         // console.log(types.results)
         return (
             <div className="type">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="row">
-                            <div className="col-md-7">
-                                <h2>Dashboard</h2>
-                                <p>User ID: {auth.user.user_id}</p>
+                {auth.isAuthenticated ? 
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <h2>Dashboard</h2>
+                                    <p>User ID: {auth.user.user_id}</p>
+                                </div>
+                                <div className="col-md-5 text-right">
+                                    <button className="btn btn-success btn-sm">Add New</button>
+                                </div>
                             </div>
-                            <div className="col-md-5 text-right">
-                                <button className="btn btn-success btn-sm">Add New</button>
-                            </div>
-                         </div>
                         </div>
                         <div className="col-md-12">
                             <table className="table table-striped table-bordered">
@@ -41,21 +42,42 @@ class Dashboard extends Component {
                                 </thead>
                                 <tbody>
                                     {types.results && types.results.map(type => (
-                                    <tr key={type.id}>
+                                        <tr key={type.id}>
                                             <td>{type.title}</td>
                                             <td>{type.color}</td>
-                                            <td> {type.is_active ? 'active' : 'Deactive'} </td>
+                                            <td> {type.is_active ? <span className="bg-success p-1 text-light">active</span> : <span className="bg-warning p-1 text-light">Deactive</span>}</td>
                                             <td>{type.created_at}</td>
                                             <td>
-                                                <button className="btn btn-info btn-sm">Update</button>
-                                                <button className="btn btn-danger ml-2 btn-sm">Delete</button>
+                                                <button
+                                                    onClick={() => console.log("Updated")} className="btn btn-info btn-sm">
+                                                    Update
+                                                </button>
+                                                <button
+                                                    onClick={() => console.log("Deleted")}
+                                                    className="btn btn-danger ml-2 btn-sm">
+                                                    Delete
+                                                </button>
                                             </td>
-                                        </tr> 
+                                        </tr>
                                     ))}
                                 </tbody>
-                        </table>
+                            </table>
+                        </div>
                     </div>
-                </div>
+                    :
+                    <div className="card text-center">
+                        <div className="card-header">
+                            <h4 className="card-title">Dashboard</h4>
+                        </div>
+                        <div className="card-body">
+                            <h1>Sorry, this is authAction</h1>
+                        </div>
+                        <div className="card-footer">
+                            <Link className="btn btn-primary" to='/login'>Login</Link>
+                            <Link className="btn btn-primary ml-2" to='/registration'>Registration</Link>
+                        </div>
+                    </div>
+                }
             </div>
         )
     }
