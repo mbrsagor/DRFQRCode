57c4cb91080b40e4c3fa61ba42527d800c131b26
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2021-05-04 22:40:31 +0600

    DjAuth redux config

--- src/components/djAuth.jsx
+++ src/components/djAuth.jsx
@@ -1,1 +1,34 @@
 
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import {connect} from 'react-redux;
+
+
+export class DjAuth extends Component {
+  
+  render() {
+    return (
+      <div className="collapse navbar-collapse" id="nav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <NavLink to="/login" activeClassName="active">
+              <span className="nav-link">Login</span>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/registration" activeClassName="active">
+              <span className="nav-link">Registration</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps => state({
+    auth: state.auth
+}) 
+
+export default connect(mapStateToProps, {}) (DjAuth);
